3fc1bb593deb1d4afa5c44c83071a3a4f18d2fb3
Nathan Shaw <n.shaw@entando.com>
2021-12-27 20:05:00 -0500

    first version

---
+++ ui/widgets/conference/detailsWidget/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,9 @@
+const conferenceApiGetResponse = {
+  name:
+    'Labore excepturi dignissimos vel nostrum aut amet. Architecto dicta ad sapiente qui. Autem nobis quae et excepturi quod iusto nobis aut et. Nihil consectetur soluta voluptatem.',
+  location:
+    'Aspernatur exercitationem sit repudiandae qui. Corrupti rem maiores molestiae magnam perferendis. Consectetur minus magnam fugit beatae. Libero adipisci provident autem soluta consequuntur neque nesciunt.',
+  date: '1995-10-21',
+};
+
+export default conferenceApiGetResponse;
---
+++ ui/widgets/conference/tableWidget/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,20 @@
+const conferenceMocks = [
+  {
+    id: 0,
+    name:
+      'Labore excepturi dignissimos vel nostrum aut amet. Architecto dicta ad sapiente qui. Autem nobis quae et excepturi quod iusto nobis aut et. Nihil consectetur soluta voluptatem.',
+    location:
+      'Aspernatur exercitationem sit repudiandae qui. Corrupti rem maiores molestiae magnam perferendis. Consectetur minus magnam fugit beatae. Libero adipisci provident autem soluta consequuntur neque nesciunt.',
+    date: '1995-10-21',
+  },
+  {
+    id: 1,
+    name:
+      'Dignissimos aperiam qui omnis fuga eius ut vel. Quis quod molestias ut. Quis et vel nam harum quas est. Natus vero ut. Repellendus non ipsa. Sit magni debitis qui.',
+    location:
+      'Hic ducimus quia a necessitatibus et quam sed. Aut dolor et id. Esse quod ipsum alias consequuntur error. Ea officiis corporis id possimus qui quibusdam vel et. Ipsam aut porro quis animi at voluptas vel.',
+    date: '1993-12-28',
+  },
+];
+
+export default conferenceMocks;
